eca05a9514c49c0c64ecc26a3f21d5b7c22415b8
Darken027 <torut1638@gmail.com>
2025-09-20 01:49:48 -0500

    Servidor web

---
+++ index.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello Word!")
+})
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => console.log(`htpp://localhost:${PORT}`));

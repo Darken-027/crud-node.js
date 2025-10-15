339b98747e8561bef2d8e1e1c330b9bcd74300f1
Darken-027 <torut1638@gmail.com>
2025-10-15 16:38:19 -0500

    Modulo producto modificado se le agrego nuevo codigo para hacer pruevas de funcionamiento

--- src/models/product.js
+++ src/models/product.js
@@ -1,21 +1,65 @@
-const { DataTypes } = require("sequelize");
-const sequelize = require("./sequelize");
+const pool = require("./mysql");
 
-const Product = sequelize.define("product", {
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  image: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  description: {
-    type: DataTypes.TEXT,
-    allowNull: true,
-  },
-});
+const store = async (name) => {
+  const sql = `INSERT INTO products (name) VALUES (?)`;
 
-Product.belongsTo(sequelize.models.category);
+  try {
+    const [result] = await pool.query(sql, [name]);
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
 
-module.exports = Product;
+const findAll = async () => {
+  const sql = "SELECT * FROM products";
+
+  try {
+    const [rows] = await pool.query(sql);
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const findById = async (id) => {
+  const sql = `SELECT * FROM products WHERE id = ?`;
+
+  try {
+    const [rows] = await pool.query(sql, [id]);
+    // console.log(rows, rows.shift())
+    return rows.shift();
+  } catch (error) {
+    throw error;
+  }
+};
+
+const update = async (id, name) => {
+  const sql = `UPDATE products SET name = ? WHERE id = ?`;
+
+  try {
+    const [result] = await pool.query(sql, [name, id]);
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const destroy = async (id) => {
+  const sql = `DELETE FROM products WHERE id = ?`;
+
+  try {
+    const [result] = await pool.query(sql, [id]);
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = {
+  store,
+  findAll,
+  findById,
+  update,
+  destroy,
+};
